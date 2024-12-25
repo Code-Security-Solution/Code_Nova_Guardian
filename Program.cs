166dfd19cf9141827f7d79b73a5c3321c4cc71e1
File <pgh268400@naver.com>
2024-12-26 02:07:55 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Nova_Guardian
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
