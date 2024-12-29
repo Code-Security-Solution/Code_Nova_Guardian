a68da1dc290187308edcdbbc3a8962b8939bff54
File <pgh268400@naver.com>
2024-12-30 03:08:35 +0900

    작업 윤곽 잡기 객체지향적 설계, 기능들은 함수로 나누는 원칙으로 설계 시작

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 작업 실행 전 필요 프로그램이 설치 되어 있나 체크
+            check_requirement();
+
+            // SonarQube 실행 로직 시작
+            process_sonar_qube();
+
+            // Semgrep 실행 로직 시작
+            process_semgrep();
+        }
+
+
+
+
+        /*
+          Main 함수가 static 함수이므로, 객체 생성 없이 바로 함수를 호출하려면 static 함수여야 한다.
+          참고 : https://jettstream.tistory.com/571
+        */
+        private static void process_sonar_qube()
+        {
+            // TODO : implement here
+        }
+        private static void process_semgrep()
+        {
+
+        }
+
+        // 해당 함수는 해당 CLI 프로그램을 사용하기 위해 필요한 프로그램들이 설치되어 있는지 검사하는 함수다.
+        private static void check_requirement()
+        {
+            throw new NotImplementedException();
         }
     }
 }
